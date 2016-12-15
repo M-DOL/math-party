abd6b54ee9688564e8c5192ac4220605194038e3
Kristen Escher <kescher@umich.edu>
2016-12-14 19:45:51 -0500

    use post request when adding new student

--- FP - Kristen/controllers/api_new_student.py
+++ FP - Kristen/controllers/api_new_student.py
@@ -5,11 +5,11 @@ from flask import *
 
 api_new_student = Blueprint('api_new_student', __name__, template_folder='templates')
 
-@api_new_student.route('/api_new_student', methods=['PUT'])
+@api_new_student.route('/api_new_student', methods=['POST'])
 def api_new_student_route():
 	
 		'''
-		PUT JSON sample
+		POST JSON sample
 		{
 			"name" : "Peter Parker"
 		}
