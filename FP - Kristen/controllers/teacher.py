50cd6262a8d19ead1c56a6dfc9469763b8655152
Kristen Escher <kescher@umich.edu>
2016-12-18 12:05:46 -0500

    implement delete class button

--- FP - Kristen/controllers/teacher.py
+++ FP - Kristen/controllers/teacher.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 teacher = Blueprint('teacher', __name__, template_folder='templates')
 
-@teacher.route('/teacher')
+@teacher.route('/teacher',  methods=['GET', 'POST'])
 def teacher_route():
-		
+	
+	if request.method == 'POST':
+		dbquery.emptyClass()
+
+
+
 	return render_template("teacher.html")
+
